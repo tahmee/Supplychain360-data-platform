28c2bcccb30377e41a96cd6b562209dded9e8019
tahmee <susantamunokubie@gmail.com>
2026-03-31 10:53:07 +0100

    Fix case statement

--- supplychain_dbt/macros/scd_type2_cols.sql
+++ supplychain_dbt/macros/scd_type2_cols.sql
@@ -1,11 +1,11 @@
 {% macro scd_type2_cols() %}
-    -- Standard SCD Type II tracking columns added to every dimension snapshot
-    dbt_scd_id         as surrogate_key,
-    dbt_valid_from     as valid_from,
-    dbt_valid_to       as valid_to,
+    -- SCD Type II tracking columns added to all dimension snapshot
+    dbt_scd_id as surrogate_key,
+    dbt_valid_from as valid_from,
+    dbt_valid_to as valid_to,
     case
         when dbt_valid_to is null then true
         else false
-    end                as is_current,
-    dbt_updated_at     as updated_at
+    end as is_current,
+    dbt_updated_at as updated_at
 {% endmacro %}
